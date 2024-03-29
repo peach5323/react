9695cc8ef5cab5c350f392a1d6e76d6dc01f429f
peach <peach5323@hotmail.com>
2024-03-29 20:51:32 +0800

    redux管理账目列表

---
+++ src/store/modules/billStore.js
@@ -0,0 +1,27 @@
+import { createSlice, dispatch } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const billStore = createSlice({
+  name: 'bill',
+  initialState: {
+    billList:[]
+  },
+  reducers: {
+    // 同步修改billList方法
+    setBillList(state, action) {
+      state.billList=action.payload
+    }
+  }
+})
+
+const { setBillList } = billStore.actions
+const getBillList = async () => {
+  // 编写异步请求
+  const res = await axios.get('http://localhost:8888/ka')
+  // 触发同步reducer
+  dispatch(setBillList(res.data))
+}
+
+export {getBillList}
+const reducer = billStore.reducer
+export default reducer
