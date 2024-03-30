2bbfd124aa5e6eab3ce969e0679b3f68e21098f4
peach <peach5323@hotmail.com>
2024-03-30 15:01:35 +0800

    月度账单-Icon组件封装

--- src/views/Month/components/DailyBill/index.js
+++ src/views/Month/components/DailyBill/index.js
@@ -1,7 +1,8 @@
 import { useMemo, useState } from 'react'
 import classNames from 'classnames'
 import './index.scss'
-import {billTypeToName} from '@/contants'
+import { billTypeToName } from '@/contants'
+import Icon from '@/components/Icon'
 
 const DailyBill = ({date, billList}) => {
 
@@ -42,6 +43,7 @@ const [visible,setVisible]=useState(false)
           {billList.map(item => {
             return (
               <div className="bill" key={item.id}>
+                <Icon type={item.useFor}></Icon>
                 <div className="detail">
                   <div className="billType">{billTypeToName[item.useFor]}</div>
                 </div>
